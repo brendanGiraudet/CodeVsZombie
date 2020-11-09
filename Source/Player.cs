560017dbebb936c308209e57b070dab9acc5e26c
baka <bakayarusama@gmail.com>
2020-11-09 23:15:56 +0100

    get info from input

--- Source/Player.cs
+++ Source/Player.cs
@@ -17,27 +17,17 @@ class Player
         // game loop
         while (true)
         {
-            inputs = Console.ReadLine().Split(' ');
-            int x = int.Parse(inputs[0]);
-            int y = int.Parse(inputs[1]);
-            int humanCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < humanCount; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int humanId = int.Parse(inputs[0]);
-                int humanX = int.Parse(inputs[1]);
-                int humanY = int.Parse(inputs[2]);
-            }
-            int zombieCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < zombieCount; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int zombieId = int.Parse(inputs[0]);
-                int zombieX = int.Parse(inputs[1]);
-                int zombieY = int.Parse(inputs[2]);
-                int zombieXNext = int.Parse(inputs[3]);
-                int zombieYNext = int.Parse(inputs[4]);
-            }
+            var me = GetMyInfo();
+            System.Console.Error.WriteLine("******** ME ********");
+            System.Console.Error.WriteLine(me);
+
+            var humans = GetHumans();
+            System.Console.Error.WriteLine("******** HUMANS ********");
+            humans.ForEach(System.Console.Error.WriteLine);
+            
+            var zombies = GetZombies();
+            System.Console.Error.WriteLine("******** ZOMBIES ********");
+            zombies.ForEach(System.Console.Error.WriteLine);
 
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
@@ -45,5 +35,75 @@ class Player
             Console.WriteLine("0 0"); // Your destination coordinates
 
         }
+    }
+    static Human GetMyInfo()
+    {
+        var inputs = Console.ReadLine().Split(' ');
+        int x = int.Parse(inputs[0]);
+        int y = int.Parse(inputs[1]);
+
+        return new Human{ Id = 0, Latitude = y, Longitude = x};
+    }
+    static List<Human> GetHumans()
+    {
+        var humans = new List<Human>();
+        int humanCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < humanCount; i++)
+            {
+                var inputs = Console.ReadLine().Split(' ');
+                humans.Add(new Human{
+                    Id=int.Parse(inputs[0]),
+                    Latitude = int.Parse(inputs[1]),
+                    Longitude = int.Parse(inputs[2])
+                });
+            }
+            return humans;
+    }
+    static List<Zombie> GetZombies()
+    {
+        var zombies = new List<Zombie>();
+        int zombieCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < zombieCount; i++)
+            {
+                var inputs = Console.ReadLine().Split(' ');
+                zombies.Add(new Zombie{
+                    Id = int.Parse(inputs[0]),
+                    Latitude = int.Parse(inputs[1]),
+                    Longitude = int.Parse(inputs[2]),
+                    NextLatitude = int.Parse(inputs[3]),
+                    NextLongitude = int.Parse(inputs[4])
+                });
+            }
+            return zombies;
+    }
+}
+class Human
+{
+    public int Id { get; set; }
+
+    public int Latitude { get; set; }
+
+    public int Longitude { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}";
+    }
+}
+class Zombie
+{
+    public int Id { get; set; }
+
+    public int Latitude { get; set; }
+
+    public int Longitude { get; set; }
+
+    public int NextLatitude { get; set; }
+
+    public int NextLongitude { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}, NextLatitude: {NextLatitude}, NextLongitude: {NextLongitude}";
     }
 }
