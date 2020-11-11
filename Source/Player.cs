6a0a1fa0906357ce85f38bc4bdd59fb68ef0edbc
baka <bakayarusama@gmail.com>
2020-11-11 17:58:30 +0100

    Add GetTheNearestZombie, GetTheNearestHuman, GetTheNearestHumanInDanger method with tests

--- Source/Player.cs
+++ Source/Player.cs
@@ -5,105 +5,170 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Save humans, destroy zombies!
- **/
-class Player
+namespace CodeVsZombie
 {
-    static void Main(string[] args)
+    /**
+    * Save humans, destroy zombies!
+    **/
+    public class Player
     {
-        string[] inputs;
-
-        // game loop
-        while (true)
+        static void Main(string[] args)
         {
-            var me = GetMyInfo();
-            System.Console.Error.WriteLine("******** ME ********");
-            System.Console.Error.WriteLine(me);
+            string[] inputs;
 
-            var humans = GetHumans();
-            System.Console.Error.WriteLine("******** HUMANS ********");
-            humans.ForEach(System.Console.Error.WriteLine);
+            // game loop
+            while (true)
+            {
+                var me = GetMyInfo();
+                System.Console.Error.WriteLine("******** ME ********");
+                System.Console.Error.WriteLine(me);
+
+                var humans = GetHumans();
+                System.Console.Error.WriteLine("******** HUMANS ********");
+                humans.ForEach(System.Console.Error.WriteLine);
+                
+                var zombies = GetZombies();
+                System.Console.Error.WriteLine("******** ZOMBIES ********");
+                zombies.ForEach(System.Console.Error.WriteLine);
+
+                var theNearestHumanInDanger = GetTheNearestHumanInDanger(me, zombies, humans);
+                System.Console.Error.WriteLine("******** NEAREST HUMAN IN DANGER ********");
+                System.Console.Error.WriteLine(theNearestHumanInDanger);
+
+                var latitudeMove = theNearestHumanInDanger.Latitude;
+                var longitudeMove = theNearestHumanInDanger.Longitude;
+
+                Console.WriteLine($"{latitudeMove} {longitudeMove}");
+            }
+        }
+        static Human GetMyInfo()
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            int x = int.Parse(inputs[0]);
+            int y = int.Parse(inputs[1]);
+
+            return new Human{ Id = 0, Latitude = y, Longitude = x};
+        }
+        static List<Human> GetHumans()
+        {
+            var humans = new List<Human>();
+            int humanCount = int.Parse(Console.ReadLine());
+                for (int i = 0; i < humanCount; i++)
+                {
+                    var inputs = Console.ReadLine().Split(' ');
+                    humans.Add(new Human{
+                        Id=int.Parse(inputs[0]),
+                        Latitude = int.Parse(inputs[1]),
+                        Longitude = int.Parse(inputs[2])
+                    });
+                }
+                return humans;
+        }
+        static List<Zombie> GetZombies()
+        {
+            var zombies = new List<Zombie>();
+            int zombieCount = int.Parse(Console.ReadLine());
+                for (int i = 0; i < zombieCount; i++)
+                {
+                    var inputs = Console.ReadLine().Split(' ');
+                    zombies.Add(new Zombie{
+                        Id = int.Parse(inputs[0]),
+                        Latitude = int.Parse(inputs[1]),
+                        Longitude = int.Parse(inputs[2]),
+                        NextLatitude = int.Parse(inputs[3]),
+                        NextLongitude = int.Parse(inputs[4])
+                    });
+                }
+                return zombies;
+        }
+
+        public static Zombie GetTheNearestZombie(Human me, List<Zombie> zombies)
+        {
+            var theNearestZombie = zombies.First();
+            var nearestDistance = Math.Abs(me.DistanceFromZero - theNearestZombie.DistanceFromZero);
+
+            zombies = zombies.Skip(1).ToList();
+
+            foreach (var zombie in zombies)
+            {
+                var distance = Math.Abs(me.DistanceFromZero - zombie.DistanceFromZero);
+
+                if(nearestDistance > distance)
+                    theNearestZombie = zombie;
+            }
+
+            return theNearestZombie;
+        }
+
+        public static Human GetTheNearestHuman(Human me, List<Human> humans)
+        {
+            var theNearestHuman = humans.First();
+            var nearestDistance = Math.Abs(me.DistanceFromZero - theNearestHuman.DistanceFromZero);
+
+            humans = humans.Skip(1).ToList();
+
+            foreach (var human in humans)
+            {
+                var distance = Math.Abs(me.DistanceFromZero - human.DistanceFromZero);
+
+                if(nearestDistance > distance)
+                    theNearestHuman = human;
+            }
+
+            return theNearestHuman;
+        }
+
+        public static Human GetTheNearestHumanInDanger(Human me, List<Zombie> zombies, List<Human> humans)
+        {
+            foreach (var human in humans)
+            {
+                var nearestZombie = GetTheNearestZombie(human, zombies);
+                var distanceFromHuman = Math.Abs(human.DistanceFromZero - nearestZombie.DistanceFromZero);
+                human.IsInDanger = distanceFromHuman < 4;
+            }
+
+            var humansInDanger = humans.Where(h => h.IsInDanger).ToList();
             
-            var zombies = GetZombies();
-            System.Console.Error.WriteLine("******** ZOMBIES ********");
-            zombies.ForEach(System.Console.Error.WriteLine);
+            if(!humansInDanger.Any())
+                humansInDanger = humans;
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
-            Console.WriteLine("0 0"); // Your destination coordinates
-
+            return GetTheNearestHuman(me, humansInDanger);
         }
     }
-    static Human GetMyInfo()
+    public class Human
     {
-        var inputs = Console.ReadLine().Split(' ');
-        int x = int.Parse(inputs[0]);
-        int y = int.Parse(inputs[1]);
+        public int Id { get; set; }
 
-        return new Human{ Id = 0, Latitude = y, Longitude = x};
+        public int Latitude { get; set; }
+
+        public int Longitude { get; set; }
+
+        public int DistanceFromZero => Latitude + Longitude;
+
+        public bool IsInDanger { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}";
+        }
     }
-    static List<Human> GetHumans()
+    public class Zombie
     {
-        var humans = new List<Human>();
-        int humanCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < humanCount; i++)
-            {
-                var inputs = Console.ReadLine().Split(' ');
-                humans.Add(new Human{
-                    Id=int.Parse(inputs[0]),
-                    Latitude = int.Parse(inputs[1]),
-                    Longitude = int.Parse(inputs[2])
-                });
-            }
-            return humans;
-    }
-    static List<Zombie> GetZombies()
-    {
-        var zombies = new List<Zombie>();
-        int zombieCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < zombieCount; i++)
-            {
-                var inputs = Console.ReadLine().Split(' ');
-                zombies.Add(new Zombie{
-                    Id = int.Parse(inputs[0]),
-                    Latitude = int.Parse(inputs[1]),
-                    Longitude = int.Parse(inputs[2]),
-                    NextLatitude = int.Parse(inputs[3]),
-                    NextLongitude = int.Parse(inputs[4])
-                });
-            }
-            return zombies;
-    }
-}
-class Human
-{
-    public int Id { get; set; }
+        public int Id { get; set; }
 
-    public int Latitude { get; set; }
+        public int Latitude { get; set; }
 
-    public int Longitude { get; set; }
+        public int Longitude { get; set; }
 
-    public override string ToString()
-    {
-        return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}";
-    }
-}
-class Zombie
-{
-    public int Id { get; set; }
+        public int NextLatitude { get; set; }
 
-    public int Latitude { get; set; }
+        public int NextLongitude { get; set; }
 
-    public int Longitude { get; set; }
+        public int DistanceFromZero => NextLatitude + NextLongitude;
 
-    public int NextLatitude { get; set; }
-
-    public int NextLongitude { get; set; }
-
-    public override string ToString()
-    {
-        return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}, NextLatitude: {NextLatitude}, NextLongitude: {NextLongitude}";
+        public override string ToString()
+        {
+            return $"Id: {Id}, Latitude: {Latitude}, Longitude: {Longitude}, NextLatitude: {NextLatitude}, NextLongitude: {NextLongitude}";
+        }
     }
 }
--- Test/PlayerTest.cs
+++ Test/PlayerTest.cs
@@ -1,14 +1,105 @@
-using System;
+
 using Xunit;
+using System.Collections.Generic;
 
 namespace CodeVsZombie.Tests
 {
-    public class UnitTest1
+    public class PlayerTest
     {
         [Fact]
-        public void Test1()
+        public void ShouldHaveTheNearestZombie()
         {
+            // Arrange
+            var me = new Human {
+                Latitude = 0,
+                Longitude = 0
+            };
+            var expectedZombie = new Zombie {
+                NextLatitude = 5,
+                NextLongitude = 8
+            };
+            var zombies = new List<Zombie>{
+                new Zombie{
+                    NextLatitude = 10,
+                    NextLongitude = 10
+                },
+                expectedZombie
+            };
 
+            // Act
+            var theNearestZombie = Player.GetTheNearestZombie(me, zombies);
+
+            // Assert
+            Assert.Equal(expectedZombie.Latitude, theNearestZombie.Latitude);
+            Assert.Equal(expectedZombie.Longitude, theNearestZombie.Longitude);
+        }
+
+        [Fact]
+        public void ShouldHaveTheNearestHuman()
+        {
+            // Arrange
+            var me = new Human {
+                Latitude = 0,
+                Longitude = 0
+            };
+            var expectedHuman = new Human {
+                Latitude = 5,
+                Longitude = 8
+            };
+            var humans = new List<Human>{
+                new Human{
+                    Latitude = 10,
+                    Longitude = 10
+                },
+                expectedHuman
+            };
+
+            // Act
+            var theNearestHuman = Player.GetTheNearestHuman(me, humans);
+
+            // Assert
+            Assert.Equal(expectedHuman.Latitude, theNearestHuman.Latitude);
+            Assert.Equal(expectedHuman.Longitude, theNearestHuman.Longitude);
+        }
+
+        [Fact]
+        public void ShouldHaveTheNearestHumanInDanger()
+        {
+            // Arrange
+            var me = new Human {
+                Latitude = 0,
+                Longitude = 0
+            };
+            
+            var zombies = new List<Zombie>{
+                new Zombie{
+                    NextLatitude = 10,
+                    NextLongitude = 10
+                },
+                new Zombie {
+                    NextLatitude = 5,
+                    NextLongitude = 8
+                }
+            };
+
+            var expectedHuman = new Human {
+                Latitude = 7,
+                Longitude = 7
+            };
+            var humans = new List<Human>{
+                new Human{
+                    Latitude = 2,
+                    Longitude = 2
+                },
+                expectedHuman
+            };
+
+            // Act
+            var theNearestHuman = Player.GetTheNearestHumanInDanger(me, zombies, humans);
+
+            // Assert
+            Assert.Equal(expectedHuman.Latitude, theNearestHuman.Latitude);
+            Assert.Equal(expectedHuman.Longitude, theNearestHuman.Longitude);
         }
     }
 }
